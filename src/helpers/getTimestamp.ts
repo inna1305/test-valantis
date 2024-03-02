7fa78d41c49d7e1023e6d4787fc3253a3543242f
Inna T <meeoow005@gmail.com>
2024-03-02 01:32:20 +0100

    fix: data obj with offset

--- src/helpers/getTimestamp.ts
+++ src/helpers/getTimestamp.ts
@@ -1,8 +1,18 @@
-const getTimestamp = (): string => {
+const getDate = () => {
     const currentDate = new Date();
+    const offsetMinutes = currentDate.getTimezoneOffset();
+    const offsetHours = offsetMinutes / 60;
+    const utcDate = new Date(currentDate.getTime() + (offsetHours * 60 * 60 * 1000));
+    console.log(utcDate);
+    return utcDate;
+}
+
+const getTimestamp = (): string => {
+   const currentDate = getDate();
     const year = currentDate.getFullYear();
     const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
     const day = currentDate.getDate().toString().padStart(2, '0');
     return '' + year + month + day;
 }
+
 export default getTimestamp;
