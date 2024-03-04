005d0a7496c25bb58593f827b2b54b8d26a3f3f1
Inna T <meeoow005@gmail.com>
2024-03-04 18:49:48 +0100

    refactor: handle button func

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,36 +8,33 @@ import {fetchData, PRODUCTS_PER_PAGE} from '../functions/requests.ts';
 const Pagination = memo((): ReactElement => {
     const context = useContext(ReducerContext);
     const [isLoading, setIsLoading] = useState(false);
-    const handleButtonPrev = () => {
-        // if (currentPage && currentPage > 1) {
-        //     reducerContext?.setValue({type: Action.stepBack, items: []})
-        // }
-    }
 
-    const isNextDisabled = (): boolean => {
-        return context!.value.items.length <= PRODUCTS_PER_PAGE || isLoading;
-    }
-
-    const handleButtonNext = () => {
+    const handleButton = (newPageNumber: number) => {
         setIsLoading(true);
-        const newCurrentPageValue = context!.value.currentPage + 1;
         const filter = context?.value.filter;
-        fetchData(newCurrentPageValue, filter!).then((items) => {
+        fetchData(newPageNumber, filter!).then((items) => {
             context!.setValue({
                 type: Action.setItems,
                 items: items,
                 nextButtonIsActive: items.length === PRODUCTS_PER_PAGE,
-                currentPage: newCurrentPageValue,
+                currentPage: newPageNumber,
             });
         })
             .then(() => setIsLoading(false));
     }
 
+    const isNextDisabled = (): boolean => {
+        //context?.value.filter !== null - stub (there is no pagination on the server for filtered products)
+        return !(context?.value.nextButtonIsActive) || context?.value.filter !== null || isLoading;
+    }
+
+    const currentPage = context?.value.currentPage;
     return (
         <Flex justify="center" gap="20px">
             <Button disabled={context?.value.currentPage === 1} icon={<LeftOutlined/>}
-                    onClick={handleButtonPrev}></Button>
-            <Button disabled={isNextDisabled()} icon={<RightOutlined/>} onClick={handleButtonNext}></Button>
+                    onClick={()=> handleButton(currentPage!-1)}></Button>
+            <Button disabled={isNextDisabled()} icon={<RightOutlined/>}
+                    onClick={()=> handleButton(currentPage!+1)}></Button>
         </Flex>);
 });
 export default Pagination;
